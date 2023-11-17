57079e6d9727da90fcf0e9f3fa8c2fc96103c7c2
kakapreter <85819721+kakapreter@users.noreply.github.com>
2023-11-17 12:47:32 +0800

    Add files via upload

---
+++ utils-test-service/tools_001/ClassMethodTypeCountUtil.java
@@ -0,0 +1,41 @@
+package cn.tedu.tool_001.util;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 统计类中所有方法返回值类型的个数
+ */
+public class ClassMethodTypeCountUtil {
+    public static Map<String, Integer> getReturnTypeCount(Class<?> clazz) {
+        Map<String, Integer> returnTypeCount = new HashMap<>();
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            if (!method.getName().startsWith("get") && !method.getName().startsWith("set")) {
+                // 忽略 getter 和 setter 方法，这样只统计普通方法的返回值类型
+                String returnType = method.getReturnType().getName();
+                returnTypeCount.put(returnType, returnTypeCount.getOrDefault(returnType, 0) + 1);
+            }
+        }
+        return returnTypeCount;
+    }
+
+    /**
+     * 传入一个类名
+     * @param className
+     */
+    public static void printClassMethodTypeCount(String className){
+        try {
+            Class<?> clazz = Class.forName(className);
+            Map<String, Integer> returnTypeCount = getReturnTypeCount(clazz);
+            System.out.println( className + "类的统计结果：");
+            for (Map.Entry<String, Integer> entry : returnTypeCount.entrySet()) {
+                System.out.println( "返回值类型为{"+ entry.getKey() + "}的方法: 共 " + entry.getValue() + " 个");
+            }
+        } catch (ClassNotFoundException e) {
+            e.getStackTrace();
+        }
+    }
+}
---
+++ utils-test-service/tools_001/TestCodeRunningSpeedUtil.java
@@ -0,0 +1,27 @@
+package cn.tedu.tool_001.util;
+
+/**
+ * 测试代码运行时间
+ */
+public class TestCodeRunningSpeedUtil {
+    public static long startMilliSecond(){
+        long current = System.currentTimeMillis();
+        System.out.println("staTime: " + current + " ms");
+        return current;
+    }
+    public static long endMilliSecond(){
+        long current = System.currentTimeMillis();
+        System.out.println("endTime: " + current + " ms");
+        return current;
+    }
+    public static long startNanSecond(){
+        long current = System.nanoTime();
+        System.out.println("staTime: " + current + " ns");
+        return current;
+    }
+    public static long endNanSecond(){
+        long current = System.nanoTime();
+        System.out.println("endTime: " + current + " ns");
+        return current;
+    }
+}
