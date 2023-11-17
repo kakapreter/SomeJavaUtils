547a5691be6267d470daa94d26343fbab7e155c3
kakapreter <85819721+kakapreter@users.noreply.github.com>
2023-11-17 13:25:48 +0800

    Update ClassMethodTypeCountUtil.java

--- utils-test-service/tools_001/ClassMethodTypeCountUtil.java
+++ utils-test-service/tools_001/ClassMethodTypeCountUtil.java
@@ -8,20 +8,41 @@ import java.util.Map;
  * 统计类中所有方法返回值类型的个数
  */
 public class ClassMethodTypeCountUtil {
-    public static Map<String, Integer> getReturnTypeCount(Class<?> clazz) {
-        Map<String, Integer> returnTypeCount = new HashMap<>();
+    static class ReturnTypeCountInfo {
+        private Map<String, Integer> returnTypeCount = new HashMap<>();
+        private int totalMethodCount = 0;
+
+        public Map<String, Integer> getReturnTypeCount() {
+            return returnTypeCount;
+        }
+
+        public int getTotalMethodCount() {
+            return totalMethodCount;
+        }
+
+        public void addReturnType(String returnType) {
+            returnTypeCount.put(returnType, returnTypeCount.getOrDefault(returnType, 0) + 1);
+        }
+
+        public void incrementTotalMethodCount() {
+            totalMethodCount++;
+        }
+    }
+    public static ReturnTypeCountInfo getReturnTypeCount(Class<?> clazz) {
+        ReturnTypeCountInfo returnTypeCountInfo = new ReturnTypeCountInfo();
         Method[] methods = clazz.getDeclaredMethods();
 
         for (Method method : methods) {
             if (!method.getName().startsWith("get") && !method.getName().startsWith("set")) {
                 // 忽略 getter 和 setter 方法，这样只统计普通方法的返回值类型
                 String returnType = method.getReturnType().getName();
-                returnTypeCount.put(returnType, returnTypeCount.getOrDefault(returnType, 0) + 1);
+                returnTypeCountInfo.addReturnType(returnType);
+                returnTypeCountInfo.incrementTotalMethodCount();
             }
         }
-        return returnTypeCount;
-    }
 
+        return returnTypeCountInfo;
+    }
     /**
      * 类的全限定名
      * @param className
@@ -29,11 +50,12 @@ public class ClassMethodTypeCountUtil {
     public static void printClassMethodTypeCount(String className){
         try {
             Class<?> clazz = Class.forName(className);
-            Map<String, Integer> returnTypeCount = getReturnTypeCount(clazz);
-            System.out.println( className + "类的统计结果：");
-            for (Map.Entry<String, Integer> entry : returnTypeCount.entrySet()) {
+            ReturnTypeCountInfo returnTypeCountInfo = getReturnTypeCount(clazz);
+            System.out.println("返回值类型统计结果：");
+            for (Map.Entry<String, Integer> entry : returnTypeCountInfo.getReturnTypeCount().entrySet()) {
                 System.out.println( "返回值类型为{"+ entry.getKey() + "}的方法：共 " + entry.getValue() + " 个");
             }
+            System.out.println("总方法数量: " + returnTypeCountInfo.getTotalMethodCount() + " 个");
         } catch (ClassNotFoundException e) {
             e.getStackTrace();
         }
